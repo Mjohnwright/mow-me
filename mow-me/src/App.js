c61f5579c0592c564eb3bc9683ff4a0aebef084f
Mjohnwright <mjohnwright@hotmail.com>
2018-06-29 10:08:15 -0400

    file structure

--- mow-me/src/App.js
+++ mow-me/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Jobs from "./pages/Jobs";
+// import Map from "./pages/Map";
 import Register from "./pages/Register";
+// import Home from "./pages/Home";
 // import Nav from "./components/Nav";
 // import Form from "./components/Form";
 
@@ -9,11 +13,13 @@ import "./App.css";
 const App = () => (
   <Router>
     <div>
-      {/* <Wrapper> */}
+      
         {/* <Route exact path="/" component={Home} /> */}
+        {/* <Route exact path="/home" component={Home} /> */}
+        {/* <Route exact path="/jobs" component={Jobs} /> */} */}
+        {/* <Route exact path="/map" component={Map} /> */} */}
         <Route exact path="/" component={Register} />
-        {/* <Route exact path="/discover" component={Jobs} /> */} */}
-      {/* </Wrapper> */}
+
     </div>
   </Router>
 );
