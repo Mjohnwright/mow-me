a1df6a451523daa4b7d6fd6f1d747b36074dcd01
Joe McCreesh <jmccreesh@libertyintegrated.com>
2018-06-30 12:26:37 -0400

    add alljobs

--- mow-me/src/App.js
+++ mow-me/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Home from "./pages/Home";
-// import Jobs from "./pages/Jobs";
+import Jobs from "./pages/Jobs";
 // import Map from "./pages/Map";
 import Register from "./pages/Register";
 // import Home from "./pages/Home";
 // import Nav from "./components/Nav";
 // import Form from "./components/Form";
-
+import AllJobs from "./components/AllJobs";
 import "./App.css";
 
 const App = () => (
@@ -16,9 +16,9 @@ const App = () => (
       
         {/* <Route exact path="/" component={Home} /> */}
         {/* <Route exact path="/home" component={Home} /> */}
-        {/* <Route exact path="/jobs" component={Jobs} /> */} */}
+        <Route exact path="/" component={Jobs} /> */}
         {/* <Route exact path="/map" component={Map} /> */} */}
-        <Route exact path="/" component={Register} />
+        {/* <Route exact path="/" component={Register} /> */}
 
     </div>
   </Router>
