964dcdcda60e97b6094fe5882960547b0adefd82
Mjohnwright <mjohnwright@hotmail.com>
2018-06-30 19:12:23 -0400

    all paths are working

---
+++ mow-me/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutUs from "../components/AboutUs";
+import Nav from "../components/Nav";
+
+const About  = () => (
+<div>
+    
+    
+    < Nav />
+    < AboutUs />
+
+
+</div>
+);
+
+export default About;
