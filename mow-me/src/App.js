dd87cbd919e8704626b18965aed1f23d499f1e58
Mjohnwright <mjohnwright@hotmail.com>
2018-06-30 18:26:49 -0400

    validation on form

--- mow-me/src/App.js
+++ mow-me/src/App.js
@@ -1,30 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-import Jobs from "./pages/Jobs";
-// import Map from "./pages/Map";
+import jobs from "./pages/jobs";
 import Register from "./pages/Register";
-// import Home from "./pages/Home";
-// import Nav from "./components/Nav";
-// import Form from "./components/Form";
-import AllJobs from "./components/AllJobs";
+import Home from "./pages/Home";
 import "./App.css";
 
 const App = () => (
   <Router>
     <div>
-      
-        {/* <Route exact path="/" component={Home} /> */}
-
-        {/* <Route exact path="/home" component={Home} /> */}
-
-
-        {/* <Route exact path="/home" component={Home} /> */}
-        <Route exact path="/" component={Jobs} /> */}
-
-          {/* <Route exact path="/home" component={Home} /> */}
-
-        {/* <Route exact path="/jobs" component={Jobs} /> */} */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} /> 
+        <Route exact path="/login" component={Register} /> 
+        <Route exact path="/jobboard" component={jobs} /> 
 
 
     </div>
