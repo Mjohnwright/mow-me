28bf2b89c3439ade7a206fdc24fe9790a90e9b24
Matt M <matthewmccarthy2@gmail.com>
2018-06-30 13:43:40 -0400

    add my components

---
+++ mow-me/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Jobs from "./pages/Jobs";
+// import Map from "./pages/Map";
+import Register from "./pages/Register";
+import About from "./pages/About";
+import Home from "./pages/Home";
+// import Nav from "./components/Nav";
+// import Form from "./components/Form";
+
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <div>
+      
+        {/* <Route exact path="/" component={Home} /> */}
+          {/* <Route exact path="/home" component={Home} /> */}
+          <Route exact path="/about" component={About} />
+        {/* <Route exact path="/jobs" component={Jobs} /> */} */}
+        {/* <Route exact path="/map" component={Map} /> */} */}
+        <Route exact path="/" component={Home} />
+
+    </div>
+  </Router>
+);
+
+export default App;
