734c9761651c6e7f95e4dca864a2950cc0f3b9b3
Mjohnwright <mjohnwright@hotmail.com>
2018-06-30 18:45:12 -0400

    validation 2

--- mow-me/src/App.js
+++ mow-me/src/App.js
@@ -1,16 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import Home from "./pages/Home";
-<<<<<<< HEAD
-// import Jobs from "./pages/Jobs";
-// import Map from "./pages/Map";
-import Register from "./pages/Register";
-import About from "./pages/About";
-import Home from "./pages/Home";
-// import Nav from "./components/Nav";
-// import Form from "./components/Form";
-
-=======
 import Jobs from "./pages/Jobs";
 // import Map from "./pages/Map";
 import Register from "./pages/Register";
@@ -18,7 +8,6 @@ import Register from "./pages/Register";
 // import Nav from "./components/Nav";
 // import Form from "./components/Form";
 import AllJobs from "./components/AllJobs";
->>>>>>> master
 import "./App.css";
 
 const App = () => (
@@ -26,13 +15,6 @@ const App = () => (
     <div>
       
         {/* <Route exact path="/" component={Home} /> */}
-<<<<<<< HEAD
-          {/* <Route exact path="/home" component={Home} /> */}
-          <Route exact path="/about" component={About} />
-        {/* <Route exact path="/jobs" component={Jobs} /> */} */}
-        {/* <Route exact path="/map" component={Map} /> */} */}
-        <Route exact path="/" component={Home} />
-=======
 
         {/* <Route exact path="/home" component={Home} /> */}
 
@@ -44,7 +26,6 @@ const App = () => (
 
         {/* <Route exact path="/jobs" component={Jobs} /> */} */}
 
->>>>>>> master
 
     </div>
   </Router>
