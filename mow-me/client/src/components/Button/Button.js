2fa0660e2626d04a7f68f9c359d314234a811c9d
Mjohnwright <mjohnwright@hotmail.com>
2018-07-10 23:00:02 -0400

    links in jumbo component

---
+++ mow-me/client/src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Button.css";
+
+const Button = props => (
+  <div>
+    <p>
+      <a className="btn btn-primary btn-lg" role="button" />
+      <a className="btn btn-primary btn-lg" role="button" />
+    </p>
+  </div>
+);
+
+export default Button;
